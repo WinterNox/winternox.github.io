5c9a3e8a0795a060cbc3709059f4656fdd42ad14
WinterNox <121163909+WinterNox@users.noreply.github.com>
2024-04-27 00:33:14 +0530

    Update discord logo and improve socials buttons

--- background.js
+++ background.js
@@ -4,12 +4,11 @@ var move_angle = 0;
 
 function increment_bg_pos() {
     move_angle += Math.PI / 3600;
-    bg_pos_x += Math.cos(move_angle);
-    bg_pos_y += Math.sin(move_angle);
+    bg_pos_x += Math.cos(move_angle) / 2;
+    bg_pos_y += Math.sin(move_angle) / 2;
 
     document.getElementById("body_element").style.backgroundPositionX = bg_pos_x + "px";
     document.getElementById("body_element").style.backgroundPositionY = bg_pos_y + "px";
-    document.getElementById("body_element").style.background
 }
 
 setInterval(increment_bg_pos, 16);
